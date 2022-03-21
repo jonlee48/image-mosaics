c69b2ca2e45f1cfd1fe708bd74f106203ddb695f
jonlee48 <jonathan.lee48@gmail.com>
2022-03-21 15:23:11 -0400

    fixed homography calclation

--- image-mosaics.py
+++ image-mosaics.py
@@ -15,34 +15,35 @@ def computeH(im1_pts, im2_pts):
 
     assert(len(im1_pts) == len(im2_pts))
 
-    for i in range(len(im1_pts)):
-        x = im1_pts[i][0]
-        y = im1_pts[i][1]
+    for im1, im2 in zip(im1_pts,im2_pts):
+        x = im1[0]
+        y = im1[1]
 
-        xp = im2_pts[i][0]
-        yp = im2_pts[i][1]
+        xp = im2[0]
+        yp = im2[1]
 
         P.append(np.array([-1*x, -1*y, -1,    0,    0,  0, x*xp, y*xp, xp]))
         P.append(np.array([   0,    0,  0, -1*x, -1*y, -1, x*yp, y*yp, yp]))
 
-    # TODO: Compute P.T H = 0
-    #zeros = np.zeros_like(P)
-    #zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    zeros = np.zeros((9))
-
-    P = np.array(P) # 9x8 for 4 points. 9x12 for 6 points
+    P = np.array(P)
     print(P)
-    print(zeros)
-    # what are the dimensions of H?
-    H = np.linalg.lstsq(P.T, zeros, rcond=None)
 
+    u, s, v = np.linalg.svd(P)
+
+    H = v[8].reshape((3,3))
+    # normalize H
+    H = (1 / H.item(8)) * H
+
+    print(H)
     return H
 
 if __name__ == '__main__':
-    im1_pts = [(1, 1, 1), (2, 2, 1), (1, 1, 1), (2, 2, 1)]#, (2, 2, 1),(2, 2, 1)]
-    im2_pts = [(5, 2, 1), (2, 8, 1), (4, 2, 1), (1, 0, 1)]#, (2, 2, 1),(2, 2, 1)]
+    im1_pts = [(0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 1)]
+    im2_pts = [(0, 0, 1), (2, 0, 1), (0, 2, 1), (2, 2, 1)]
 
     H = computeH(im1_pts, im2_pts)
 
+    for p in im1_pts:
+        p = np.array(p)
+        print(H@p)
 
-    print(H)
