03c676616741c47e90b9c72f0b6f911fac6238a1
jonlee48 <jonathan.lee48@gmail.com>
2022-03-21 13:08:42 -0400

    working on least squares

--- image-mosaics.py
+++ image-mosaics.py
@@ -25,18 +25,24 @@ def computeH(im1_pts, im2_pts):
         P.append(np.array([-1*x, -1*y, -1,    0,    0,  0, x*xp, y*xp, xp]))
         P.append(np.array([   0,    0,  0, -1*x, -1*y, -1, x*yp, y*yp, yp]))
 
-    # TODO: Compute PH = 0    
-    zeros = np.zeros_like(P)
-    zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # TODO: Compute P.T H = 0
+    #zeros = np.zeros_like(P)
+    #zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zeros = np.zeros((9))
 
-    H = np.linalg.lstsq(P, zeros, rcond=None)
+    P = np.array(P) # 9x8 for 4 points. 9x12 for 6 points
+    print(P)
+    print(zeros)
+    # what are the dimensions of H?
+    H = np.linalg.lstsq(P.T, zeros, rcond=None)
 
     return H
 
 if __name__ == '__main__':
-    im1_pts = [(1, 1), (2, 2)]
-    im2_pts = [(1, 1), (2, 2)]
+    im1_pts = [(1, 1, 1), (2, 2, 1), (1, 1, 1), (2, 2, 1)]#, (2, 2, 1),(2, 2, 1)]
+    im2_pts = [(5, 2, 1), (2, 8, 1), (4, 2, 1), (1, 0, 1)]#, (2, 2, 1),(2, 2, 1)]
 
     H = computeH(im1_pts, im2_pts)
+
 
     print(H)
