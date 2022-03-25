2dadf3496ce88622f6357bd448aacefcb44c44d5
jonlee48 <jonathan.lee48@gmail.com>
2022-03-25 17:11:00 -0400

    proper sized mosaic created with image 2 added

--- gui.py
+++ gui.py
@@ -1,49 +1,85 @@
 import pygame
+import cv2
 import numpy as np
 from mosaics import computeH
 
-def resize_img(img, display_width, display_height):
-    ''' Scale the image to fit within display size, maintain aspect ratio '''
-    width_scale = display_width / img.get_width()
-    height_scale = display_height / img.get_height()
+def resize_img(im, display_width, display_height):
+    '''
+    Scale the image to fit within display size, maintain aspect ratio
+        im: pygame image
+        display_width, display_height: ints
+        returns: resized img
+    '''
+    width_scale = display_width / im.get_width()
+    height_scale = display_height / im.get_height()
     scale = min(width_scale, height_scale)
 
-    resize = (img.get_width() * scale, img.get_height() * scale)
-    img_resize = pygame.transform.scale(img, resize)
+    resize = (im.get_width() * scale, im.get_height() * scale)
+    img_resize = pygame.transform.scale(im, resize)
     return img_resize
 
+def resize_to_fit(im_width, im_height, display_width, display_height):
+    '''
+    Return dimensions of the image to fit within display size while maintaining aspect ratio
+        im_width, im_height: dimensions of image
+        display_width, display_height: dimensions of display
+        returns: (int_width, int_height)
+    '''
+    width_scale = display_width / im_width
+    height_scale = display_height / im_height
+    scale = min(width_scale, height_scale)
+
+    resize = (int(im_width * scale), int(im_height * scale))
+    return resize
+
+
 # input images to align
-img1_path = 'imgs/sea-lion.jpg'
-img2_path = 'imgs/cat.jpg'
+im1_path = 'imgs/lodge1.jpg'
+im2_path = 'imgs/lodge2.jpg'
+
+# start the pygame interface
+pygame.init()
+clock = pygame.time.Clock()
+
+
+''' Resize images to fit on the screen '''
+im1_raw = cv2.imread(im1_path)
+im2_raw = cv2.imread(im2_path)
+
+# get screen dimensions
+info = pygame.display.Info()
+display_width = info.current_w
+display_height = info.current_h - 100
+
+im1_new_size = resize_to_fit(im1_raw.shape[1], im1_raw.shape[0], display_width, display_height)
+im1_resize = cv2.resize(im1_raw, im1_new_size)
+im2_new_size = resize_to_fit(im2_raw.shape[1], im2_raw.shape[0], display_width, display_height)
+im2_resize = cv2.resize(im2_raw, im2_new_size)
+
+# save back to file so it can be opened by pygame
+name, format = im1_path.split('.')
+im1_newpath = name + '_resized.' + format
+name, format = im2_path.split('.')
+im2_newpath = name + '_resized.' + format
+
+cv2.imwrite(im1_newpath, im1_resize)
+cv2.imwrite(im2_newpath, im2_resize)
 
 state = 0
 features = []
 colors = [(255,0,0), (0,255,0), (0,0,255), (255,0,255)]
-display_height = 600
-display_width = 600
+
 
 # load images into pygame
-img1_orig = pygame.image.load(img1_path)
-img2_orig = pygame.image.load(img2_path)
+im1 = pygame.image.load(im1_newpath)
+im2 = pygame.image.load(im2_newpath)
 
-# resize images
-img1 = resize_img(img1_orig, display_width, display_height)
-img2 = resize_img(img2_orig, display_width, display_height)
 
-img1_resize = img1.get_size()
-img2_resize = img2.get_size()
 
-# Clock
-clock = pygame.time.Clock()
-
-# start the pygame interface
-pygame.init()
-screen = pygame.display.set_mode((display_width, display_height))
-
-# Background Color
-screen.fill((0, 0, 0))
+# start display with first image
+screen = pygame.display.set_mode((im1.get_width(), im1.get_height())) # window dimensions same as image
 # Show the image
-screen.blit(img1, (0,0))
+screen.blit(im1, (0,0))
 
 
 quit = False
@@ -58,8 +94,10 @@ while not quit:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if state == 4:
                 # transition to next image
-                screen.fill((0, 0, 0))
-                screen.blit(img2, (0, 0))
+                screen = pygame.display.set_mode((im2.get_width(), im2.get_height()))
+                # Show the image
+                screen.blit(im2, (0, 0))
+
 
             elif state < 9:
                 # one of the 8 features manually selected
@@ -74,7 +112,6 @@ while not quit:
                 features.append(pos)
 
             elif state == 9:
-                screen.fill((0, 0, 0))
 
                 # create two 4x2 matrix for set of features
                 im1_pts = np.array(features[:4])
@@ -92,13 +129,89 @@ while not quit:
                 print(H)
 
                 # TODO: show mosaic
-                screen.blit(img2, (0, 0))
+                ''' Compute size of np array needed to fit mosaic '''
+                # get coords of corners of im1 after mapping
+                corners = [(0,0,1),
+                           (im1.get_width(),0,1),
+                           (0,im1.get_height(),1),
+                           (im1.get_width(),im1.get_height(),1)]
+
+                corners_mapped = np.zeros((4,2))
+                for i,c in enumerate(corners):
+                    cp = H@c
+                    # normalize by 3rd coordinate
+                    cp = cp/cp[2]
+                    corners_mapped[i] = cp[0:2]
+
+                #print(corners_mapped)
+
+
+                # get lower and upper bounds of im1 and 2
+                w_low = min(np.amin(corners_mapped[:,0]), 0)
+                w_up  = max(np.amax(corners_mapped[:,0]), im2.get_width())
+                h_low = min(np.amin(corners_mapped[:,1]), 0)
+                h_up  = max(np.amax(corners_mapped[:,1]), im2.get_height())
+
+
+                # get coordinates of origin (top,left corner) for im2
+                origin_w = int(-w_low)
+                origin_h = int(-h_low)
+
+
+                # setup empty array for mosaic
+                width = int(w_up-w_low)
+                height = int(h_up-h_low)
+                mosaic = np.zeros((height,width,3))
+                print(mosaic.shape)
+                print(im2.get_width())
+                print(im2.get_height())
+
+
+                ''' add image 2 to mosaic '''
+                # image 2 needs to be same dimensions as mosaic
+                im2 = cv2.imread(im2_newpath)
+                print(im2.shape)
+
+                im2_width = im2.shape[1]+origin_w
+                im2_height = im2.shape[0]+origin_h
+                pad_width = ((int(origin_h), int(mosaic.shape[0] - im2_height)), (int(origin_w), int(mosaic.shape[1] - im2_width)),(0,0))
+                print('pad_width')
+                print(pad_width)
+                im2_padded = np.pad(im2,
+                                    pad_width=pad_width,
+                                    mode='constant',
+                                    constant_values=0)
+                # TODO: the width and height may still differ by 1 (due to rounding)
+                print('padded shape')
+                print(im2_padded.shape)
+
+                print('mosaic shape')
+                print(mosaic.shape)
+
+                mosaic += im2_padded
+
+                mosaic_path = 'imgs/mosaic.png'
+                cv2.imwrite(mosaic_path,mosaic)
+
+
+                # Load im1 as np array
+                pygame_mosaic = pygame.image.load(mosaic_path)
+
+                ''' display the result '''
+                screen = pygame.display.set_mode((mosaic.shape[1], mosaic.shape[0]))
+                screen.fill((0, 0, 0))
+                screen.blit(pygame_mosaic, (0, 0))
+
+                # Show the mapped 4 points from im1
                 for i, p in enumerate(im1_pts):
                     pp = H@p
+                    # normalize by 3rd coordinate
                     pp = pp/pp[2]
-                    x, y, _ = pp
+                    w, h, _ = pp
+                    w -= origin_w
+                    h -= origin_h
                     color = colors[i%4]
-                    pygame.draw.circle(screen, color, (x,y), radius=5)
+                    pygame.draw.circle(screen, color, (w,h), radius=5)
 
             state += 1
 
